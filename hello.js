6901a04d4aa1d2cd7d4cf5307377515a39aa4de3
Bianca-Kn <1921825@stud.hs-mannheim.de>
2022-06-22 20:26:36 +0200

    bereinigte mavi daten enthalten

--- hello.js
+++ hello.js
@@ -17,9 +17,9 @@ function main() {
     var g = svg.append("g")
         .attr("transform", "translate(" + 100 + "," + 100 + ")");
 
-    d3.csv("Mavi006.csv").then( function(data) {
-        xScale.domain(data.map(function(d) { return d.timestamp; }));
-        yScale.domain([0, d3.max(data, function(d) { return 200000; })]);
+    d3.csv("csv_mavi_sum.csv").then( function(data) {
+        xScale.domain(data.map(function(d) { return d.timestamp }));
+        yScale.domain([0, d3.max(data, function(d) { return 300000; })]);
 
         g.append("g")
             .attr("transform", "translate(0," + height + ")")
@@ -32,11 +32,22 @@ function main() {
             .text("timestamp");
 
         g.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(xScale))
+            .selectAll("text")
+            .style("text-anchor", "end")
+            .attr("dx", "-.8em")
+            .attr("dy", ".15em")
+            .attr("transform", function(d) {
+                return "rotate(-75)"
+            });
+        g.append("g")
             .call(d3.axisLeft(yScale).tickFormat(function(d){return d;}).ticks(10))
             .append("text")
             .attr("transform", "rotate(-90)")
             .attr("y", 10)
-            //.attr('dy', '-5em')
+            .attr('dy', '-5em')
             .attr('text-anchor', 'end')
             .attr('stroke', 'black')
             .text('Anzahl Fahrzeuge')
@@ -47,13 +58,14 @@ function main() {
             .attr("class", "bar")
             .on("mouseover", onMouseOver) // Add listener for event
             .on("mouseout", onMouseOut)
-            .attr("x", function(d) { return xScale(d.timestamp); })
+            .attr("x", function(d) { return xScale((d).timestamp); })
             .attr("y", function(d) { return yScale(d.value); })
             .attr("width", xScale.bandwidth())
-            .transition()
-            .ease(d3.easeLinear)
-            .duration(500)
-            .delay(function(d,i){ return i * 50})
+
+           // .transition()
+          //  .ease(d3.easeLinear)
+         //   .duration(500)
+       //     .delay(function(d,i){ return i * 50})
             .attr("height", function(d) { return height - yScale(d.value); });
     })
 
@@ -73,14 +85,14 @@ function main() {
         d3.select('#tooltip').classed('hidden', false);
 
 
-       /* d3.select(this).attr('class','highlight')
+        d3.select(this).attr('class','highlight')
         d3.select(this)
             .transition() // I want to add animnation here
             .duration(500)
             .attr('width',function(d) {return weight - xScale(d.timestamp)})
             .attr('y', function(d){return yScale(d.value) - 10;})
             .attr('height', function(d){return height - yScale(d.value) + 10;})
-*/
+
     }
 
     // Mouseout event handler
